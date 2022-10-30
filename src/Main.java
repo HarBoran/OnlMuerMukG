9c8f1dcefbb8df8cec1e0c55f5f526f9102d1b88
ms <soe3676@nate.com>
2022-10-30 22:40:38 +0900

    javax

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,44 @@
-import java.awt.Toolkit;
-
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start OnlMuerMukG Project");
-        Toolkit toolkit= Toolkit.getDefaultToolkit();
 
-        for(int i=0; i<5; i++) {
-            toolkit.beep();
-            try {
-                Thread.sleep(1000);
-                //Thread.sleep(1000)은 () 안에 1/1000(초) 단위 이므로, 1초 간격으로 들리고 찍는다.
-            } catch (Exception e) {
-                System.out.println(e);
-                //Thread가 들어간 문장은 반드시 예외처리를 해야 한다. 잠시 Thread 처리를 1초간 멈춰라 ~
-            }
-        }
+        // ★ 프레임 생성
+        JFrame frm = new JFrame();
+
+        // ★ 프레임 제목 설정
+        frm.setTitle("슬라임 퇴치하기");
+
+        // 프레임 크기 설정
+        frm.setSize(350, 300);
+
+        // 프레임을 화면 가운데에 배치
+        frm.setLocationRelativeTo(null);
+
+        // 프레임을 닫았을 때 프로그램이 종료되도록 설정
+        frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // 레이아웃 설정
+        frm.getContentPane().setLayout(null);
+
+        // 버튼 생성
+        JButton btn1 = new JButton("슬라삐");
+        JButton btn2 = new JButton("슬라디");
+
+        // 버튼 위치와 크기 설정
+        btn1.setBounds(30, 170, 122, 30);
+        btn2.setBounds(182, 170, 122, 30);
+
+        // 프레임에다가 버튼 추가
+        frm.getContentPane().add(btn1);
+        frm.getContentPane().add(btn2);
+
+        // ★ 라벨 설정
+        JLabel lbl = new JLabel();
+        lbl.setBounds(30, 200, 274, 50);
+        lbl.setText("게임을 시작합니다");
+        frm.getContentPane().add(lbl);
+
+        // 프레임이 보이도록 설정
+        frm.setVisible(true);
     }
 }
